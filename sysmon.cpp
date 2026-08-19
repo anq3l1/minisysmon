5b5b8e5cd90bfcc1285078639a4079bbdb4ee821
angel <142012420+anq3l1@users.noreply.github.com>
2026-08-19 03:38:58 +0300

    added files

---
+++ sysmon.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "system.h"
+#include <iomanip>
+#include <thread>
+#include <chrono>
+
+int main()
+{
+while(true)
+{    
+    std::cout << "╭──────────── SYSTEM MONITOR ────────────╮\n";
+
+    std::cout << "│ " << std::left << std::setw(10)
+              << "CPU"
+              << cpu_stat() << "%"
+    << std::right << std::setw(30) << "│" << "\n";
+
+    std::cout << "│ " << std::left << std::setw(10)
+              << "GPU"
+              << gpu_stat() << "%"
+    << std::right << std::setw(30) << "│" << "\n";
+
+    std::cout << "│ " << std::left << std::setw(10)
+              << "RAM"
+              << used_ram() << " / "
+              << totalram() << " GB"
+    << std::right << std::setw(12) << "│" << "\n";
+
+    std::cout << "│ " << std::left << std::setw(10)
+              << "CPU Temp"
+              << cpu_temp() / 1000 << "°C"
+    << std::right << std::setw(28) << "│" << "\n";
+
+    std::cout << "│ " << std::left << std::setw(10)
+              << "Uptime"
+              << sys_uptime() << " min"
+    << std::right << std::setw(26) << "│" << "\n";
+
+    std::cout << "╰────────────────────────────────────────╯\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "\033[2J\033[H";
+}
+
+    return 0;
+}
---
+++ system.h
@@ -0,0 +1,99 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <iostream>
+#include <sys/sysinfo.h>
+#include <fstream>
+#include <thread>
+#include <chrono>
+
+int sys_uptime()
+{
+    struct sysinfo si;
+    int uptime = 0;
+
+    if(sysinfo(&si) == 0)
+    {
+        uptime = si.uptime / 60;
+    }
+
+    return uptime;
+}
+
+long long cpu_stat()
+{
+    std::string cpu;
+    long long user, nice, system, idle, iowait, irq, softirq, steal, total1, total2, idle1, idle2, delta_total, delta_idle;
+
+    std::ifstream file("/proc/stat");
+    file >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+    
+    total1 = user + nice + system + idle + iowait + irq + softirq + steal;
+
+    idle1 = idle + iowait;
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::ifstream file2("/proc/stat");
+    
+    file2 >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+    
+    total2 = user + nice + system + idle + iowait + irq + softirq + steal;
+    
+    idle2 = idle + iowait;        
+    
+    delta_total = total2 - total1;
+    
+    delta_idle = idle2 - idle1;
+
+    long long usage = 100 * (delta_total - delta_idle) / delta_total;
+
+    return usage;
+}
+
+int gpu_stat()
+{
+    std::ifstream file("/sys/class/drm/card1/device/gpu_busy_percent");
+    std::string line;
+
+    file >> line;
+
+    int perc = std::stoi(line);
+
+    return perc;
+}
+
+float used_ram()
+{
+    struct sysinfo si;
+
+    if(sysinfo(&si) == 0)
+    {
+        return (float)(si.totalram - si.freeram)
+               / 1024 / 1024 / 1024;
+    }
+
+    return 0;
+}
+float totalram()
+{
+    struct sysinfo si;
+    if(sysinfo(&si) == 0)
+    {
+        return (float)si.totalram * si.mem_unit / 1024 / 1024 / 1024;
+    }
+
+    return 0;
+}
+
+int cpu_temp()
+{
+    std::ifstream file("/sys/class/hwmon/hwmon4/temp1_input");
+
+    int temp;
+
+    file >> temp;
+
+    return temp;
+}
+
+#endif
